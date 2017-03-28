1e2924520481e87c78321b0f45a8ac34567c1282
Terrell Harris <tmharris0126@gmail.com>
2017-03-28 17:31:28 -0500

    second commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,35 @@
 export default function app() {
-	let $chatcontainer = $('#chatcontainer');
-	let $chatroom = $('.chatroom');
-	let input = $('#login-input');
-	let form = $('#login-form');
+    let $chatcontainer = $('#chatcontainer');
+    let $chatroom = $('.chatroom');
+    let input = $('#login-input');
+    let form = $('#login-form');
+    let storeuser = [];
+    let activeusers;
 
 
-	$(form).on('submit', function(e) {
-		e.preventDefault();
-		input.val()
+    $('form button').on('click', function(e) {
+        e.preventDefault();
+				// Store the username from the form
+        activeusers = input.val()
 
-		$.ajax({
-			type: 'POST',
+        //Show the hidden 'chatroom container'
+				$('.chatroom').removeClass('hide-page');
+				$('.chatroom #chatwindow').append(activeusers);
 
-		}).then(
+				//Hide the "login container"
+				$('.mainmenu').addClass('hide-page');
+				$('.mainmenu .homepage').append('')
 
-		)
-	});
-};
+				//Load the messages for the chat using ajax
+    });
+
+		//Add event handler on message send
+		//Add setInterval to refresh messages in chat container
+}
+//   $.ajax({
+//       type: 'POST',
+//       url: 'http://tiny-za-server.herokuapp.com/collections/thatguyschat/',
+//       data: $(this).serialize(),
+//       dataType: 'json',
+// 			success: function( resp ) {
+// console.log( resp );
